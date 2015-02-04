60dec6e5c13a83fbf5b550c9e6af1992b2b92d6c
Georgios Exarchakos <g.exarchakos@tue.nl>
2015-02-04 17:17:08 +0100

    DefectsMcM: small cleaning in retrieved fields

--- DefectsMcM/App.js
+++ DefectsMcM/App.js
@@ -27,7 +27,6 @@ Ext.define('CustomApp', {
 			filters: defectsFilter,
 			listeners: {
 				load: function(store, data, success) {
-					//console.log(data);
 					for(var i=0; i<data.length; i++) {
 						console.log(data[i].data);
 						//if(data[i].data.Project && data[i].data.Project!==null)
@@ -49,7 +48,7 @@ Ext.define('CustomApp', {
 				}
 				//scope:this
 			},
-			fetch: ['FormattedID', 'Project', 'Parent', 'ScheduleState', 'CreationDate', 'AcceptedDate', 'ClosedDate', 'InProgressDate', 'OpenedDate', 'TargetDate', 'RevisionHistory', 'LastUpdateDate', 'PlanEstimate', 'Deliverysatisfactionscore110', 'Teamstatus', 'TeamLifecycle', 'LPCVelocity', 'Release', 'ProductOwnerInvolvement15']
+			fetch: ['Project', 'ScheduleState', 'CreationDate', 'AcceptedDate', 'ClosedDate', 'InProgressDate', 'OpenedDate', 'TargetDate', 'RevisionHistory', 'LastUpdateDate']
 		});
 		
 	},
@@ -92,144 +91,5 @@ Ext.define('CustomApp', {
 			]
 		});
 		this.add(this.grid);
-	},
-	
-	loadTeams: function(teams) {
-		var teamsFilter = null;
-		for(var team=0; team<teams.length; team++) {
-			var tmp = Ext.create('Rally.data.wsapi.Filter', {
-					property: 'Name',
-					operator : "=",
-					value: teams[team]
-				});
-			if(teamsFilter===null) teamsFilter = tmp;
-			else teamsFilter = teamsFilter.or(tmp);
-		}
-
-		var teamStore = Ext.create('Rally.data.wsapi.Store', {
-			model: 'Project',
-			filters: teamsFilter,
-			autoLoad: true,
-			listeners: {
-				load: function(teamStore, teamData, success) {
-					//console.log('got data!', teamStore, teamData, success);
-					this.loadSprints(teamData, this);
-				},
-				scope: this
-			},
-			fetch: ['Name']
-		});
-	},
-	
-	loadSprints: function(team_data, app) {
-		var sprintsFilter = null;
-		for(var team=0; team<team_data.length; team++) {
-			var tmp = Ext.create('Rally.data.wsapi.Filter', {
-					property: 'Project.Name',
-					operator : "=",
-					value: team_data[team].data.Name
-				});
-			if(sprintsFilter===null) sprintsFilter = tmp;
-			else sprintsFilter = sprintsFilter.or(tmp);
-		}
-		var today = new Date();
-		sprintsFilter = sprintsFilter.and(Ext.create('Rally.data.wsapi.Filter', {
-				property: 'EndDate',
-				operator : "<=",
-				value: today
-			})
-		);
-		Ext.create('Rally.data.wsapi.Store', {
-			autoLoad : true,
-			model: 'Iteration',
-			filters: sprintsFilter,
-			sorters: [ {property:'EndDate', direction:'DESC'}],
-			listeners: {
-				load: function(store, data, success) {
-					var items = [];
-					var storyFilter = null;
-					for(var t=0; t<team_data.length; t++) {
-						var program = team_data[t].data.Parent.Name;
-						var team = team_data[t].data.Name;
-						var j = 0;
-						for(var s=0; s<data.length; s++) {
-							//console.log('Sprint:', data[s].data)
-							if(data[s].data.Project.Name==team) {
-								//console.log('Sprint:', program, team, data[s].data.Name);
-								var tmpFilter = Ext.create('Rally.data.wsapi.Filter', {
-										property: 'Iteration.ObjectID',
-										operator: '=',
-										value: data[s].data.ObjectID
-									}).and(Ext.create('Rally.data.wsapi.Filter', {
-										property: 'Parent.Name',
-										operator: 'Contains',
-										value: 'Iteration Reporting Parent'
-									}).or(Ext.create('Rally.data.wsapi.Filter', {
-										property: 'ScheduleState',
-										operator: '=',
-										value: 'Accepted'
-									}))).and(Ext.create('Rally.data.wsapi.Filter', {
-										property: 'DirectChildrenCount',
-										operator: '=',
-										value: '0'
-									}));
-								if(storyFilter===null) storyFilter = tmpFilter;
-								else storyFilter = storyFilter.or(tmpFilter);
-								items.push({team: team_data[t].data, sprint: data[s].data,'velocities':[data[s].data.PlannedVelocity,0,0,0]});
-								j++;
-								if(j>=app.sprints_per_team)
-									break;
-							}
-						}
-					}
-					app.loadStories(storyFilter, items);
-				},
-				scope : app
-			},
-			fetch: ['Name', 'Project', 'ObjectID','PlannedVelocity','StartDate','EndDate']
-		});
-	},
-	
-	loadStories: function(filter, items) {
-		Ext.create('Rally.data.wsapi.Store', {
-			autoLoad : true,
-			model: 'HierarchicalRequirement',
-			filters: filter,
-			listeners: {
-				load: function(store, data, success) {
-					var defectFilter = null;
-					for(var i=0; i<items.length; i++) {
-						//console.log(items[i]['sprint']['Name']);
-						for(var v=0; v<data.length; v++) {
-							if(data[v].data.Iteration._refObjectUUID==items[i].sprint._refObjectUUID) {
-								if(data[v].data.Parent && data[v].data.Parent!==null && data[v].data.Parent._refObjectName.indexOf('Iteration Reporting Parent')>-1) {
-									items[i].velocities[1]=data[v].get('LPCVelocity');
-									items[i].sprint.satisfaction=data[v].get('Deliverysatisfactionscore110');
-									
-								} else {
-									items[i].velocities[2]+=data[v].get('PlanEstimate');
-								}
-							}
-						}
-						var tmpFilter = Ext.create('Rally.data.wsapi.Filter', {
-							property: 'Iteration.ObjectID',
-							operator: '=',
-							value: items[i].sprint.ObjectID
-						}).and(Ext.create('Rally.data.wsapi.Filter', {
-							property: 'ScheduleState',
-							operator: '=',
-							value: 'Accepted'
-						}));
-						if(defectFilter===null) defectFilter = tmpFilter;
-						else defectFilter = defectFilter.or(tmpFilter);
-					}
-					this.loadDefects(defectFilter, items);						
-				},
-				scope:this
-			},
-			fetch: ['FormattedID', 'Parent', 'ScheduleState', 'Iteration', 'PlanEstimate', 'Deliverysatisfactionscore110', 'Teamstatus', 'TeamLifecycle', 'LPCVelocity', 'Release', 'ProductOwnerInvolvement15']
-		});
-		
 	}
-	
 });
